5f5dadbcc532e65805d629716c8f191250c0abf2
799034160 <799034160@qq.com>
2019-06-27 18:12:39 +0800

    第一次修改

--- sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient  //启用发现客户端
 @SpringBootApplication
 public class Sp02ItemserviceApplication {
-
+    //改个注释呗
 	public static void main(String[] args) {
 		SpringApplication.run(Sp02ItemserviceApplication.class, args);
 	}
